c5bb1df18e2cab3ccd2fbb493fa1facf3f4bf23f
clau4774 <claudiodangiolo@gmail.com>
2021-02-18 09:53:17 -0300

    I finished the main functionality but I still need to do the arrow rotation

--- code.js
+++ code.js
@@ -1,1 +1,13 @@
-console.log('Hola mundo')
+let selection = document.getElementsByClassName('select');
+
+for (let i = 0; i < selection.length; i++) {
+    selection[i].addEventListener('click', e => {
+        e.preventDefault();
+        let content = e.target.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + 'px';
+        }
+    })
+};
