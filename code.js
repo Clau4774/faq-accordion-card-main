ac048abe61f47852b3f8290668065aa0ecff68c8
clau4774 <claudiodangiolo@gmail.com>
2021-02-18 16:16:41 -0300

    Javascript ready, doing the desktop layout

--- code.js
+++ code.js
@@ -1,13 +1,25 @@
-let selection = document.getElementsByClassName('select');
+const select = document.querySelectorAll('.select');
+const contain = document.querySelectorAll('.contain');
+const arrows = document.querySelectorAll('.arrow');
+const collapse = document.querySelectorAll('.collapse')
 
-for (let i = 0; i < selection.length; i++) {
-    selection[i].addEventListener('click', e => {
+for (let i = 0; i < contain.length; i++) {
+    contain[i].addEventListener('click', e => {
         e.preventDefault();
-        let content = e.target.nextElementSibling;
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
+
+        if (collapse[i].style.height) {
+            collapse[i].style.height = null;
         } else {
-            content.style.maxHeight = content.scrollHeight + 'px';
+            collapse[i].style.height = collapse[i].scrollHeight + 'px';
         }
+
+        if (arrows[i].style.transform) {
+            arrows[i].style.transform = null;
+        } else {
+            arrows[i].style.transform = 'rotate(180deg)';
+        }
+
+        select[i].classList.toggle('bold');
     })
 };
+
